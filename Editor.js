49f40052d1d6279e0901b4d9d19a4bbe67a14d0f
Mustafa Yücel <mustafayucel@gmail.com>
2020-01-07 13:48:08 +0300

    Script removed before dumping html

--- Editor.js
+++ Editor.js
@@ -1,88 +1,93 @@
+var myScriptId = "my_jquery_script_s1";
+var jqueryVersion = "3.4.1";
 (function() {
-  // the minimum version of jQuery we want
-  var v = "3.4.1";
-
-  // check prior inclusion and version
-  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-    var done = false;
-    var script = document.createElement("script");
-    script.src =
-      "https://cdnjs.cloudflare.com/ajax/libs/jquery/" + v + "/jquery.js";
-    script.onload = script.onreadystatechange = function() {
-      if (
-        !done &&
-        (!this.readyState ||
-          this.readyState == "loaded" ||
-          this.readyState == "complete")
-      ) {
-        done = true;
-        initMyBookmarklet();
-      }
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-  } else {
-    initMyBookmarklet();
-  }
-
-  function initMyBookmarklet() {
-    (window.myBookmarklet = function() {
-      myFunc();
-    })();
-  }
-})();
-
-function myFunc() {
-  var $dragging, backColor;
-
-  function setElem($elem) {
-    resetElem(); // reset previous element
-    $dragging = $elem;
-    backColor = $dragging.css("background-color");
-    $dragging.css("background-color", "yellow");
-  }
-  function resetElem() {
-    if (!$dragging) return;
-    $dragging.css("background-color", backColor);
-    $dragging = null;
-  }
-  function dumpHtml() {
-    resetElem();
-    console.log($("html").html());
-  }
-
-  $(document.body).on("click", function(e) {
-    if (e.ctrlKey) {
-      dumpHtml();
-      return;
+    // the minimum version of jQuery we want
+    var v = jqueryVersion;
+  
+    // check prior inclusion and version
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+      var done = false;
+      var script = document.createElement("script");
+      script.id = myScriptId;
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/jquery/" + v + "/jquery.js";
+      script.onload = script.onreadystatechange = function() {
+        if (
+          !done &&
+          (!this.readyState ||
+            this.readyState == "loaded" ||
+            this.readyState == "complete")
+        ) {
+          done = true;
+          initMyBookmarklet();
+        }
+      };
+      document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+      initMyBookmarklet();
     }
-    // Body must have given a height!
-    if (e.target == document.body) {
+  
+    function initMyBookmarklet() {
+      (window.myBookmarklet = function() {
+        myFunc();
+      })();
+    }
+  })();
+  
+  function myFunc() {
+    var $dragging, backColor;
+  
+    function setElem($elem) {
+      resetElem(); // reset previous element
+      $dragging = $elem;
+      backColor = $dragging.css("background-color");
+      $dragging.css("background-color", "yellow");
+    }
+    function resetElem() {
+      if (!$dragging) return;
+      $dragging.css("background-color", backColor);
+      $dragging = null;
+    }
+    function dumpHtml() {
       resetElem();
-      return;
+      var html = $("html").html();
+      html = html.replace('<script id="' + myScriptId + '" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/'  + jqueryVersion + '/jquery.js"></script>', "");      
+      console.log(html);
     }
-    setElem($(e.target));
-    //console.log($dragging[0].nodeName);
-
-    return false;
-  });
-
-  $(document).on("keydown", function(e) {
-    if (!$dragging) return;
-    $elem = $dragging;
-    //console.log($elem.css("top") + " " + $elem.position().top);
-    if (e.which == "38") {
-      // up arrow
-      $elem.css("top", $elem.position().top - 1 + "px");
-    } else if (e.which == "40") {
-      // top arrow
-      $elem.css("top", $elem.position().top + 1 + "px");
-    } else if (e.which == "37") {
-      // left arrow
-      $elem.css("left", $elem.position().left - 1 + "px");
-    } else if (e.which == "39") {
-      // right arrow
-      $elem.css("left", $elem.position().left + 1 + "px");
-    }
-    return false;
-  });
-}
+  
+    $(document.body).on("click", function(e) {
+      if (e.ctrlKey) {
+        dumpHtml();
+        return;
+      }
+      // Body must have given a height!
+      if (e.target == document.body) {
+        resetElem();
+        return;
+      }
+      setElem($(e.target));
+      //console.log($dragging[0].nodeName);
+  
+      return false;
+    });
+  
+    $(document).on("keydown", function(e) {
+      if (!$dragging) return;
+      $elem = $dragging;
+      //console.log($elem.css("top") + " " + $elem.position().top);
+      if (e.which == "38") {
+        // up arrow
+        $elem.css("top", $elem.position().top - 1 + "px");
+      } else if (e.which == "40") {
+        // top arrow
+        $elem.css("top", $elem.position().top + 1 + "px");
+      } else if (e.which == "37") {
+        // left arrow
+        $elem.css("left", $elem.position().left - 1 + "px");
+      } else if (e.which == "39") {
+        // right arrow
+        $elem.css("left", $elem.position().left + 1 + "px");
+      }
+      return false;
+    });
+  }
