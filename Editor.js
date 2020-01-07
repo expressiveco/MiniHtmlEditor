6d79926c931251a7148339fc61a568496f736dcc
Mustafa Yücel <mustafayucel@gmail.com>
2020-01-07 10:01:58 +0300

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -1,96 +1,88 @@
-(function(){
+(function() {
+  // the minimum version of jQuery we want
+  var v = "3.4.1";
 
-    // the minimum version of jQuery we want
-    var v = "3.4.1";
-
-    // check prior inclusion and version
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/" + v + "/jquery.js";
-        script.onload = script.onreadystatechange = function(){
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                initMyBookmarklet();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
+  // check prior inclusion and version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/jquery/" + v + "/jquery.js";
+    script.onload = script.onreadystatechange = function() {
+      if (
+        !done &&
+        (!this.readyState ||
+          this.readyState == "loaded" ||
+          this.readyState == "complete")
+      ) {
+        done = true;
         initMyBookmarklet();
-    }
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
 
-    function initMyBookmarklet() {
-        (window.myBookmarklet = function() {
-            myFunc();
-        })();
-    }
-
+  function initMyBookmarklet() {
+    (window.myBookmarklet = function() {
+      myFunc();
+    })();
+  }
 })();
 
 function myFunc() {
   var $dragging, backColor;
-  
-  function setElem($elem)
-  {
+
+  function setElem($elem) {
     resetElem(); // reset previous element
     $dragging = $elem;
     backColor = $dragging.css("background-color");
     $dragging.css("background-color", "yellow");
-    
   }
-  function resetElem()
-  {
-    if (!$dragging)
-      return;
+  function resetElem() {
+    if (!$dragging) return;
     $dragging.css("background-color", backColor);
     $dragging = null;
   }
-  function dumpHtml()
-  {
-      resetElem();
-      console.log($("html").html());
-    
+  function dumpHtml() {
+    resetElem();
+    console.log($("html").html());
   }
 
-
-$(document.body).on("click", function(e){
-  if (e.ctrlKey) {
+  $(document.body).on("click", function(e) {
+    if (e.ctrlKey) {
       dumpHtml();
       return;
-  }
-  // Body must have given a height!
-  if (e.target == document.body)
-    {
+    }
+    // Body must have given a height!
+    if (e.target == document.body) {
       resetElem();
       return;
     }
-  setElem($(e.target))
-  console.log($dragging[0].nodeName);
-  
-  return false;
-});
+    setElem($(e.target));
+    console.log($dragging[0].nodeName);
 
-$(document).on("keydown", function (e) {
-  if (!$dragging)
-    return;
+    return false;
+  });
+
+  $(document).on("keydown", function(e) {
+    if (!$dragging) return;
     $elem = $dragging;
     //console.log($elem.css("top") + " " + $elem.position().top);
-    if (e.which == '38') {
-        // up arrow
-      $elem.css("top", ($elem.position().top - 1) + "px" );
-    }
-    else if (e.which == '40') {
-        // top arrow
-        $elem.css("top", ($elem.position().top + 1) + "px" );
-    }
-    else if (e.which == '37') {
-       // left arrow
-      $elem.css("left", ($elem.position().left - 1) + "px" );
-    }
-    else if (e.which == '39') {
-       // right arrow
-      $elem.css("left", ($elem.position().left + 1) + "px" );
+    if (e.which == "38") {
+      // up arrow
+      $elem.css("top", $elem.position().top - 1 + "px");
+    } else if (e.which == "40") {
+      // top arrow
+      $elem.css("top", $elem.position().top + 1 + "px");
+    } else if (e.which == "37") {
+      // left arrow
+      $elem.css("left", $elem.position().left - 1 + "px");
+    } else if (e.which == "39") {
+      // right arrow
+      $elem.css("left", $elem.position().left + 1 + "px");
     }
     return false;
-});
+  });
 }
